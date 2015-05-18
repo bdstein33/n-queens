9256b752970673453c82a4950ce4ab85195aa042
Hack Reactor Students <accounts@hackreactor.com>
2015-05-18 15:26:26 -0700

    Completed hasAnyRowConflicts function

--- src/Board.js
+++ src/Board.js
@@ -99,12 +99,22 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
-      return false; // fixme
+      var board = this.rows();
+
+      var count = 0;
+      for(var row = 0; row < board.length; row++) {
+        count += board[row][colIndex];
+      }
+      // debugger;
+      return count > 1; // fixme
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      return false; // fixme
+      var board = this;
+      return this.rows().reduce(function(prevVal, col, colNum){
+        return prevVal || board.hasColConflictAt(colNum);
+      }, false);
     },
 
 
