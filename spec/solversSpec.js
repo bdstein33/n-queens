7918900ad91dccee250e3cf0ba26c98b9db04a5b
Hack Reactor Students <accounts@hackreactor.com>
2015-05-19 19:30:10 -0700

    Revert to original

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -3,7 +3,7 @@ describe('solvers', function() {
 
   describe('findNRooksSolution()', function() {
 
-    xit('finds a valid solution for n of 1-8', function() {
+    it('finds a valid solution for n of 1-8', function() {
       _.range(1, 9).map(function(n) {
         var solutionBoard = new Board(findNRooksSolution(n));
         var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
@@ -22,7 +22,7 @@ describe('solvers', function() {
 
   describe('countNRooksSolutions()', function() {
 
-    xit('finds the number of valid solutions for n of 1-8', function() {
+    it('finds the number of valid solutions for n of 1-8', function() {
       _.range(1, 9).map(function(n) {
         var solutionCount = countNRooksSolutions(n);
         var expectedSolutionCount = [1, 1, 2, 6, 24, 120, 720, 5040, 40320][n];
@@ -35,22 +35,22 @@ describe('solvers', function() {
 
   describe('findNQueensSolution()', function() {
 
-    xit('finds a valid solution for n of 0-7', function() {
+    it('finds a valid solution for n of 0-7', function() {
       // Skip 2 and 3 because they have no solution.
-      // [1, 4, 5, 6, 7, 8].map(function(n) {
-      //  var solutionBoard = new Board(findNQueensSolution(n));
-      //   var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
-      //     return memo + _.reduce(row, function(memo, col) {
-      //       return memo + col;
-      //     }, 0);
-      //   }, 0);
+      [0, 1, 4, 5, 6, 7, 8].map(function(n) {
+        var solutionBoard = new Board(findNQueensSolution(n));
+        var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
+          return memo + _.reduce(row, function(memo, col) {
+            return memo + col;
+          }, 0);
+        }, 0);
 
-      //   expect(solutionBoard.get('n')).to.equal(n);
-      //   expect(numPieces).to.equal(n);
-      //   expect(solutionBoard.hasAnyQueensConflicts()).to.be.equal(false);
-      // });
+        expect(solutionBoard.get('n')).to.equal(n);
+        expect(numPieces).to.equal(n);
+        expect(solutionBoard.hasAnyQueensConflicts()).to.be.equal(false);
+      });
 
-      //Check 2 and 3 for no solution
+      // Check 2 and 3 for no solution
       [2, 3].map(function (n) {
         var solutionBoard = new Board(findNQueensSolution(n));
         var numPieces = _.reduce(solutionBoard.rows(), function(memo, row) {
@@ -69,13 +69,14 @@ describe('solvers', function() {
   describe('countNQueensSolutions()', function() {
 
     it('finds the number of valid solutions for n of 0-8', function() {
-      _.range(4,9).map(function(n) {
+      _.range(0, 9).map(function(n) {
         var solutionCount = countNQueensSolutions(n);
-        var expectedSolutionCount = [1, 0, 0, 2, 10, 4, 40, 92][n];
+        var expectedSolutionCount = [1, 1, 0, 0, 2, 10, 4, 40, 92][n];
 
         expect(solutionCount).to.be.equal(expectedSolutionCount);
       });
     });
 
   });
+
 });
